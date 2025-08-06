838f16b02d5532648f092d72a7db97261509fa75
Jordan Arizanov <jordan.arizanov@auroralabs.com>
2025-08-06 21:29:15 +0200

    Add environment variable mapping from JORDAN to LOCI variables

--- .github/actions/insights/index.js
+++ .github/actions/insights/index.js
@@ -3,6 +3,14 @@ const exec = require("@actions/exec");
 
 async function run() {
   try {
+    // Map JORDAN environment variables to LOCI variables that loci_api expects
+    if (process.env.JORDAN_API_TOKEN) {
+      process.env.LOCI_API_KEY = process.env.JORDAN_API_TOKEN;
+    }
+    if (process.env.JORDAN_BACKEND_URL) {
+      process.env.LOCI_BACKEND_URL = process.env.JORDAN_BACKEND_URL;
+    }
+
     const project = core.getInput("project", { required: true });
     const version = core.getInput("version", { required: true });
 
--- .github/actions/upload/dist/index.js
+++ .github/actions/upload/dist/index.js
@@ -27561,6 +27561,14 @@ const path = __nccwpck_require__(6928);
 
 async function run() {
   try {
+    // Map JORDAN environment variables to LOCI variables that loci_api expects
+    if (process.env.JORDAN_API_TOKEN) {
+      process.env.LOCI_API_KEY = process.env.JORDAN_API_TOKEN;
+    }
+    if (process.env.JORDAN_BACKEND_URL) {
+      process.env.LOCI_BACKEND_URL = process.env.JORDAN_BACKEND_URL;
+    }
+
     const iProject  = core.getInput("project", { required: true });
     const iVersion  = core.getInput("version", { required: true });
     const iBinaries = core.getInput("binaries", { required: true });
